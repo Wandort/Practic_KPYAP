64681527835ac43c262303512da5524a85fd02dd
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-15 14:10:28 +0300

    Сделал 2 задание

---
+++ Arabok_A/Zad_1/Zad_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int digit1 = number % 10;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number / 100;
+
+            int reversedNumber = digit1 * 100 + digit2 * 10 + digit3;
+
+            Console.WriteLine($"Число, полученное при прочтении цифр справа налево: {reversedNumber}");
+        }
+    }
+}
