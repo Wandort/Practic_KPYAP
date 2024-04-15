1a96dc4573ce96d0ad3b39de1241bd27cc0711e6
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-15 13:47:28 +0300

    Create repos and Tema A

---
+++ Arabok_A/Zad_1/Zad_1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 целых числа");
+
+            Console.WriteLine("Введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число:");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("$a");
+            
+            Console.ReadLine();
+        }
+    }
+}
