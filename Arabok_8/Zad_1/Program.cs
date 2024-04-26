b4e4202cdebb9fcf4df175dbdbbb6f3945a4b2ec
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-26 13:23:59 +0300

    Готовая 8 тема

---
+++ Arabok_8/Zad_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+// Определение структуры STUDENT
+struct STUDENT
+{
+    public string SurnameAndInitials;
+    public int GroupNumber;
+    public int[] Grades;
+}
+
+class Program
+{
+    static void Main()
+    {
+        STUDENT[] students = new STUDENT[2];
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine($"Введите данные для студента {i + 1}:");
+
+            Console.Write("Фамилия и инициалы: ");
+            students[i].SurnameAndInitials = Console.ReadLine();
+
+            Console.Write("Номер группы: ");
+            students[i].GroupNumber = int.Parse(Console.ReadLine());
+
+            students[i].Grades = new int[5];
+            for (int j = 0; j < students[i].Grades.Length; j++)
+            {
+                Console.Write($"Оценка {j + 1}: ");
+                students[i].Grades[j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Array.Sort(students, (a, b) => a.SurnameAndInitials.CompareTo(b.SurnameAndInitials));
+
+        bool found = false;
+        foreach (var student in students)
+        {
+            if (student.Grades.Contains(2))
+            {
+                Console.WriteLine($"Студент: {student.SurnameAndInitials}, Группа: {student.GroupNumber}");
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Нет студентов с оценкой 2.");
+        }
+    }
+}
---
+++ Arabok_8/Zad_2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Accountant
+{
+    public bool AskForBonus(string worker, int hours)
+    {
+        int standardHours = 140;
+        if (hours > standardHours)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Accountant accountant = new Accountant();
+
+        Console.Write("Введите имя сотрудника: ");
+        string workerName = Console.ReadLine();
+
+        Console.Write("Введите количество отработанных часов: ");
+        int workedHours = int.Parse(Console.ReadLine());
+
+        bool bonusEligible = accountant.AskForBonus(workerName, workedHours);
+
+        if (bonusEligible)
+        {
+            Console.WriteLine($"{workerName} имеет право на премию.");
+        }
+        else
+        {
+            Console.WriteLine($"{workerName} не имеет права на премию.");
+        }
+    }
+}
