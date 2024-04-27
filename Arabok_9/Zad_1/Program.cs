6e7af7ebe43de2691b65501af9ddddc3a9c0dd4f
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-27 10:16:43 +0300

    Готовая 9 тема

---
+++ Arabok_9/Zad_1/Program.cs
@@ -0,0 +1,59 @@
+﻿interface Iy
+{
+    int IxF0(int x);
+    int IxF1(int x);
+}
+
+interface Iz
+{
+    int F0(char c);
+    int F1(char c);
+}
+
+class MyClass : Iy, Iz
+{
+    public int IxF0(int x)
+    {
+        return x + 5;
+    }
+
+    public int IxF1(int x)
+    {
+        return x + 5;
+    }
+
+    public int F0(char c)
+    {
+        if (char.IsLetter(c))
+        {
+            return c - 'a' + 5;
+        }
+        return 0;
+    }
+
+    public int F1(char c)
+    {
+        if (char.IsLetter(c))
+        {
+            return 5;
+        }
+        return 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myClass = new MyClass();
+
+        Iy iy = myClass;
+        Iz iz = myClass;
+
+        int result1 = iy.IxF0(10);
+        int result2 = iz.F0('b');
+
+        Console.WriteLine($"Результат 1: {result1}");
+        Console.WriteLine($"Результат 2: {result2}");
+    }
+}
---
+++ Arabok_9/Zad_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Car : IComparable<Car>
+{
+    public string LicensePlate { get; set; }
+    public string Color { get; set; }
+    public string OwnerLastName { get; set; }
+    public bool IsPresent { get; set; }
+
+    public int CompareTo(Car other)
+    {
+        return LicensePlate.CompareTo(other.LicensePlate);
+    }
+}
+
+class ParkingLot
+{
+    private List<Car> cars = new List<Car>();
+
+    public void AddCar(Car car)
+    {
+        cars.Add(car);
+    }
+
+    public Car FindCarByLicensePlate(string licensePlate)
+    {
+        return cars.Find(car => car.LicensePlate == licensePlate);
+    }
+
+    public List<Car> GetPresentCars()
+    {
+        return cars.FindAll(car => car.IsPresent);
+    }
+
+    public List<Car> GetAbsentCars()
+    {
+        return cars.FindAll(car => !car.IsPresent);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ParkingLot parkingLot = new ParkingLot();
+
+        parkingLot.AddCar(new Car { LicensePlate = "ABC123", Color = "Red", OwnerLastName = "Smith", IsPresent = true });
+        parkingLot.AddCar(new Car { LicensePlate = "XYZ789", Color = "Blue", OwnerLastName = "Johnson", IsPresent = false });
+
+        Car foundCar = parkingLot.FindCarByLicensePlate("ABC123");
+        if (foundCar != null)
+        {
+            Console.WriteLine($"Найден автомобиль: {foundCar.Color} {foundCar.LicensePlate}");
+        }
+        else
+        {
+            Console.WriteLine("Автомобиль не найден.");
+        }
+
+        List<Car> presentCars = parkingLot.GetPresentCars();
+        List<Car> absentCars = parkingLot.GetAbsentCars();
+
+        Console.WriteLine("Присутствующие автомобили:");
+        foreach (var car in presentCars)
+        {
+            Console.WriteLine($"{car.Color} {car.LicensePlate}");
+        }
+
+        Console.WriteLine("Отсутствующие автомобили:");
+        foreach (var car in absentCars)
+        {
+            Console.WriteLine($"{car.Color} {car.LicensePlate}");
+        }
+    }
+}
