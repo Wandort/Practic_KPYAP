ab9be536977a1450a0bc74d0329adda656a53573
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-30 12:46:16 +0300

    Готовая 10 тема

---
+++ Arabok_10/Zad_1/Program.cs
@@ -0,0 +1,61 @@
+﻿class Book
+{
+    private string title;
+    private double price;
+
+    public Book(string title, double price)
+    {
+        this.title = title;
+        this.price = price;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public double GetPrice()
+    {
+        return price;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Название: {title}, Цена: {price} руб.");
+    }
+}
+
+class HardcoverBook : Book
+{
+    private string coverMaterial;
+
+    public HardcoverBook(string title, double price, string coverMaterial)
+        : base(title, price)
+    {
+        this.coverMaterial = coverMaterial;
+    }
+
+    public void DisplayCoverInfo()
+    {
+        Console.WriteLine($"Материал обложки: {coverMaterial}");
+    }
+
+    public new void DisplayInfo()
+    {
+        base.DisplayInfo();
+        DisplayCoverInfo();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book book = new Book("Преступление и наказание", 500);
+
+        HardcoverBook hardcoverBook = new HardcoverBook("Мастер и Маргарита", 700, "твердая");
+
+        book.DisplayInfo();
+        hardcoverBook.DisplayInfo();
+    }
+}
---
+++ Arabok_10/Zad_2/Program.cs
@@ -0,0 +1,44 @@
+﻿class A
+{
+    private int a;
+    private int b;
+
+    public A()
+    {
+        a = 10;
+        b = 20;
+    }
+
+    public int C
+    {
+        get { return a + b; }
+    }
+}
+
+class B : A
+{
+    private int d;
+
+    public B() : base()
+    {
+        d = 30;
+    }
+
+    public int C2
+    {
+        get { return C + d; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A objA = new A();
+
+        B objB = new B();
+
+        Console.WriteLine($"Свойство C в классе A: {objA.C}");
+        Console.WriteLine($"Свойство C2 в классе B: {objB.C2}");
+    }
+}
