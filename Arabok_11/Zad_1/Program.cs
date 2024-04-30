54c37caece1c05a1c4aeb0feac206b396da3a638
Wandort <92667198+Wandort@users.noreply.github.com>
2024-04-30 13:15:28 +0300

    Готовая 11 тема

---
+++ Arabok_11/Zad_1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Settlement
+{
+    public string Name { get; set; }
+
+    public Settlement(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void DisplayResults()
+    {
+        Console.WriteLine("Населенный пункт: " + Name);
+    }
+}
+
+class Village : Settlement
+{
+    public int NumberOfHouses { get; set; }
+    public int ResidentsPerHouse { get; set; }
+    public double Area { get; set; }
+
+    public Village(string name, int numberOfHouses, int residentsPerHouse, double area) : base(name)
+    {
+        NumberOfHouses = numberOfHouses;
+        ResidentsPerHouse = residentsPerHouse;
+        Area = area;
+    }
+
+    public override void DisplayResults()
+    {
+        base.DisplayResults();
+        Console.WriteLine("Количество домов: " + NumberOfHouses);
+        Console.WriteLine("Жителей на дом: " + ResidentsPerHouse);
+        Console.WriteLine("Площадь: " + Area);
+    }
+
+    public double PopulationDensity()
+    {
+        return (NumberOfHouses * ResidentsPerHouse) / Area;
+    }
+}
+
+class City : Settlement
+{
+    public int NumberOfResidents { get; set; }
+    public double Area { get; set; }
+
+    public City(string name, int numberOfResidents, double area) : base(name)
+    {
+        NumberOfResidents = numberOfResidents;
+        Area = area;
+    }
+
+    public override void DisplayResults()
+    {
+        base.DisplayResults();
+        Console.WriteLine("Количество жителей: " + NumberOfResidents);
+        Console.WriteLine("Площадь: " + Area);
+    }
+
+    public double PopulationDensity()
+    {
+        return NumberOfResidents / Area;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Settlement> settlements = new List<Settlement>();
+
+        settlements.Add(new Village("Деревня А", 50, 4, 100));
+        settlements.Add(new City("Город X", 1000, 500));
+
+        foreach (Settlement settlement in settlements)
+        {
+            settlement.DisplayResults();
+            if (settlement is Village)
+            {
+                Village village = (Village)settlement;
+                Console.WriteLine("Плотность населения: " + village.PopulationDensity());
+            }
+            else if (settlement is City)
+            {
+                City city = (City)settlement;
+                Console.WriteLine("Плотность населения: " + city.PopulationDensity());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Arabok_11/Zad_2/Program.cs
@@ -0,0 +1,52 @@
+﻿abstract class FuelConsumption
+{
+    public string VehicleName { get; set; }
+    public double FuelUsed { get; set; }
+
+    public abstract double CalculateFuelConsumption();
+}
+
+class Car : FuelConsumption
+{
+    public Car(string name, double fuelUsed)
+    {
+        VehicleName = name;
+        FuelUsed = fuelUsed;
+    }
+
+    public override double CalculateFuelConsumption()
+    {
+        return FuelUsed;
+    }
+}
+
+class Truck : FuelConsumption
+{
+    public Truck(string name, double fuelUsed)
+    {
+        VehicleName = name;
+        FuelUsed = fuelUsed;
+    }
+
+    public override double CalculateFuelConsumption()
+    {
+        return FuelUsed;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        FuelConsumption[] vehicles = new FuelConsumption[]
+        {
+            new Car("Toyota Camry", 10.5),
+            new Truck("Volvo FH16", 25.0),
+        };
+
+        foreach (var vehicle in vehicles)
+        {
+            Console.WriteLine($"Транспортное средство: {vehicle.VehicleName}, Использовано топлива: {vehicle.CalculateFuelConsumption()} литров");
+        }
+    }
+}
