d6d74c8031e7b37d360866c45893f4b8bf824439
Wandort <92667198+Wandort@users.noreply.github.com>
2024-05-11 09:14:38 +0300

    Готовая 21

---
+++ Arabok_21/Zad_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+abstract class State
+{
+    public abstract void Handle(Context context);
+}
+
+class StateA : State
+{
+    public override void Handle(Context context)
+    {
+        Console.WriteLine("Обработка состояния StateA");
+        context.State = new StateB();
+    }
+}
+
+class StateB : State
+{
+    public override void Handle(Context context)
+    {
+        Console.WriteLine("Обработка состояния StateB");
+        context.State = new StateA();
+    }
+}
+
+class Context
+{
+    public State State { get; set; }
+
+    public Context(State state)
+    {
+        this.State = state;
+    }
+
+    public void Request()
+    {
+        this.State.Handle(this);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Context context = new Context(new StateA());
+        context.Request();
+        context.Request();
+    }
+}
