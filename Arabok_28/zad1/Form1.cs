f46cb76831693766ee8c19fd815da6c822b8751e
Wandort <92667198+Wandort@users.noreply.github.com>
2024-05-21 13:02:49 +0300

    28
    28

---
+++ Arabok_28/zad1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Point[] trianglePoints = { new Point(30, 70), new Point(90, 20), new Point(150, 70) };
+            g.DrawPolygon(Pens.Red, trianglePoints);
+
+            Rectangle ellipseRect = new Rectangle(170, 30, 100, 80);
+            g.DrawEllipse(Pens.Blue, ellipseRect);
+
+            Rectangle filledCircleRect = new Rectangle(300, 30, 70, 70);
+            g.FillEllipse(Brushes.Orange, filledCircleRect);
+
+            Rectangle filledRectangleRect = new Rectangle(390, 30, 100, 80);
+            g.FillRectangle(Brushes.Purple, filledRectangleRect);
+
+            Rectangle sectorRect = new Rectangle(510, 30, 100, 80);
+            g.FillPie(Brushes.Yellow, sectorRect, 0, 120);
+
+            Rectangle targetRect = new Rectangle(30, 150, 100, 100);
+            g.DrawEllipse(Pens.Green, targetRect);
+            g.DrawEllipse(Pens.Green, targetRect.X + 25, targetRect.Y + 25, 50, 50);
+            g.DrawEllipse(Pens.Green, targetRect.X + 50, targetRect.Y + 50, 0, 0);
+
+            int squareSize = 30;
+            for (int i = 0; i < 4; i++)
+            {
+                Rectangle squareRect = new Rectangle(180 + i * squareSize, 180 + i * squareSize, squareSize, squareSize);
+                g.FillRectangle(Brushes.LightBlue, squareRect);
+                g.DrawRectangle(Pens.Red, squareRect);
+            }
+
+            int gridSize = 15;
+            for (int row = 0; row < 6; row++)
+            {
+                for (int col = 0; col < 6; col++)
+                {
+                    Brush brush = (row + col) % 2 == 0 ? Brushes.Gray : Brushes.LightGray;
+                    Rectangle cellRect = new Rectangle(350 + col * gridSize, 180 + row * gridSize, gridSize, gridSize);
+                    g.FillRectangle(brush, cellRect);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Arabok_28/zad2/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zad2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class TestDrawing : Control
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            Point p1 = new Point(60, 60);
+            Point p2 = new Point(120, 140);
+            Point p3 = new Point(180, 60);
+            Pen trianglePen = new Pen(Brushes.Green, 3);
+            drawingContext.DrawLine(trianglePen, p1, p2);
+            drawingContext.DrawLine(trianglePen, p2, p3);
+            drawingContext.DrawLine(trianglePen, p3, p1);
+
+            Rect ellipseRect = new Rect(new Point(230, 70), new Size(120, 60));
+            Pen ellipsePen = new Pen(Brushes.Orange, 3);
+            drawingContext.DrawEllipse(null, ellipsePen, new Point(290, 100), 60, 30);
+
+            Rect filledCircleRect = new Rect(new Point(370, 70), new Size(60, 60));
+            Brush filledCircleBrush = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+            drawingContext.DrawEllipse(filledCircleBrush, null, new Point(400, 100), 30, 30);
+
+            Rect filledRectangleRect = new Rect(new Point(470, 70), new Size(120, 60));
+            Brush filledRectangleBrush = new SolidColorBrush(Color.FromRgb(128, 0, 128));
+            drawingContext.DrawRectangle(filledRectangleBrush, null, filledRectangleRect);
+
+            Point center = new Point(620, 100);
+            double radius = 60;
+            double startAngle = 0;
+            double endAngle = 90;
+            StreamGeometry sectorGeometry = new StreamGeometry();
+            using (StreamGeometryContext context = sectorGeometry.Open())
+            {
+                context.BeginFigure(center + new Vector(radius, 0), true, true);
+                context.ArcTo(center, new Size(radius, radius), 0, false, SweepDirection.Clockwise, true, true);
+                context.LineTo(center, true, true);
+            }
+            Pen sectorPen = new Pen(Brushes.Yellow, 3);
+            drawingContext.DrawGeometry(new SolidColorBrush(Color.FromRgb(255, 0, 0)), sectorPen, sectorGeometry);
+
+            Rect square1 = new Rect(new Point(80, 200), new Size(40, 40));
+            Rect square2 = new Rect(new Point(110, 230), new Size(40, 40));
+            Rect square3 = new Rect(new Point(140, 260), new Size(40, 40));
+            Rect square4 = new Rect(new Point(170, 290), new Size(40, 40));
+            Rect square5 = new Rect(new Point(200, 320), new Size(40, 40));
+            Pen squarePen = new Pen(Brushes.DarkBlue, 2);
+            drawingContext.DrawRectangle(new SolidColorBrush(Color.FromRgb(173, 216, 230)), squarePen, square1);
+            drawingContext.DrawRectangle(new SolidColorBrush(Color.FromRgb(173, 216, 230)), squarePen, square2);
+            drawingContext.DrawRectangle(new SolidColorBrush(Color.FromRgb(173, 216, 230)), squarePen, square3);
+            drawingContext.DrawRectangle(new SolidColorBrush(Color.FromRgb(173, 216, 230)), squarePen, square4);
+            drawingContext.DrawRectangle(new SolidColorBrush(Color.FromRgb(173, 216, 230)), squarePen, square5);
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    Rect cellRect = new Rect(new Point(350 + i * 40, 200 + j * 40), new Size(40, 40));
+                    Brush cellBrush = (i + j) % 2 == 0 ? Brushes.LightGray : Brushes.DarkGray;
+                    drawingContext.DrawRectangle(cellBrush, null, cellRect);
+                }
+            }
+        }
+    }
+
+}
